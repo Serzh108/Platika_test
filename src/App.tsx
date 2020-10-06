83f8a46799fff4637d96e3ba1170900e10a8f491
Serzh108 <serzh@gabris.kiev.ua>
2020-10-06 11:55:52 +0300

    predeploy version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Scoreboard from './components/Scoreboard/Scoreboard';
-import logo from './logo.svg';
 import logoPlaytika from './logo-playtika.webp';
 import './App.css';
 
@@ -8,11 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src={logoPlaytika} className="App-logo" alt="logo" />
-        <p>Title</p>
-        <Scoreboard />
       </header>
+      <Scoreboard />
     </div>
   );
 }
