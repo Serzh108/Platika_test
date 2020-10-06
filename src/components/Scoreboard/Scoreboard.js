d5407c86ab161b9db18fdccc982fd567635684df
Serzh108 <serzh@gabris.kiev.ua>
2020-10-06 12:46:06 +0300

    add loader

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -1,52 +1,69 @@
-import Axios from 'axios';
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { css } from '@emotion/core';
+import RingLoader from 'react-spinners/RingLoader';
 import styles from './Scoreboard.module.css';
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+`;
 
 const initialState = {
   balance: 0,
-  // bets: [],
   last_bet: 0,
-  rolls: [[0], [0], [0]],
+  rolls: [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ],
   uid: 0,
   win: 0,
 };
 
+axios.defaults.baseURL = 'https://game-server.kovalevskyi.net';
+
 export default function Scoreboard() {
   const [state, setState] = useState(initialState);
   const [bets, setBets] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getData();
   }, []);
 
   const clickPlayHandler = () => {
-    console.log('Buttom clicked!!!');
     placeBet();
+    setIsLoading(true);
   };
 
   const placeBet = async () => {
-    const betUrl = `https://game-server.kovalevskyi.net/spin?uid=102&bet=${state.last_bet}`;
+    const betUrl = `/spin?uid=103&bet=${state.last_bet}`;
     try {
-      const response = await Axios.get(betUrl);
+      const response = await axios.get(betUrl);
       console.log('response-2: ', response.data);
       const win = response.data.hasOwnProperty('win') ? response.data.win : 0;
       setState(prev => ({ ...prev, ...response.data, win }));
-      // setState(prev => ({ ...prev, last_bet: response.data.last_bet, balance: response.data.balance }));
     } catch (error) {
       console.log('error: ', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const getData = async () => {
-    const initUrl = 'https://game-server.kovalevskyi.net/init?uid=102';
+    const initUrl = `/init?uid=103`;
     try {
-      const response = await Axios.get(initUrl);
+      const response = await axios.get(initUrl);
       console.log('response: ', response.data);
       setState(prev => ({ ...prev, ...response.data }));
       setBets(response.data.bets);
       // setState(prev => ({ ...prev, last_bet: response.data.last_bet, balance: response.data.balance }));
     } catch (error) {
       console.log('error: ', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -59,6 +76,17 @@ export default function Scoreboard() {
 
   return (
     <>
+      {isLoading && (
+        <div className="sweet-loading" style={{ position: 'absolute' }}>
+          <RingLoader
+            css={override}
+            size={220}
+            color={'#ff6c00'}
+            loading={isLoading}
+          />
+        </div>
+      )}
+
       <div className={styles.container}>
         <p>win: {state.win}</p>
         <p>balance: {state.balance}</p>
